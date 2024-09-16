31cc622e4e046888dafd7bacd45ec00b4935adc4
Andre Hartman <andre.hartman@doane.edu>
2024-09-15 22:05:02 -0500

    Pricing Engine with unit tests

---
+++ PricingEngine/PricingProgram.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PricingEngineApp
+{
+    internal class PricingProgram
+    {
+        static void Main(string[] args)
+        {
+            bool holiday = false;
+            string holidayInput = "";
+            bool exitApp = false;
+
+            Console.WriteLine("Welcome to the Pricing Engine");
+            Console.WriteLine("_____________________________");
+
+
+            
+            while (!exitApp)
+            {
+                Console.Write("Is today a Holiday? (y or n): ");
+                holidayInput = Console.ReadLine().ToLower();
+
+                if(holidayInput == "y" || holidayInput == "n")
+                {
+                    if (holidayInput == "y")
+                    {
+                        holiday = true;
+                    }
+                    
+                    bool exitLoop = false;
+                    PricingEngine engine = new PricingEngine(holiday);
+
+                    while (!exitLoop)
+                    {
+                        int quantity;
+                        decimal price;
+
+                        Console.Write("Enter quantity: ");
+                        string quantityStr = Console.ReadLine();
+                        while(!int.TryParse(quantityStr, out quantity))
+                        {
+                            Console.Write("Invalid input, please enter a number: ");
+                            quantityStr = Console.ReadLine();
+                        }
+
+                        Console.Write("Enter Price: ");
+                        string priceStr = Console.ReadLine();
+                        while(!decimal.TryParse(priceStr, out price))
+                        {
+                            Console.Write("Invalid input, please enter a number: ");
+                            priceStr = Console.ReadLine();
+                        }
+
+                        Console.Write("Your unit price: ");
+                        Console.WriteLine($"$ {engine.CalculateUnitPrice(quantity, price)}");
+
+                        Console.Write("Calculate another item? (y or n): ");
+                        string addItem = Console.ReadLine();
+
+                        if (addItem == "y")
+                        {
+
+                        }
+                        else
+                        {
+                            exitLoop = true;
+                            exitApp = true;
+                        }
+                    }
+                    
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input, try again ");
+                }
+    
+               
+            }
+            
+
+
+
+
+            
+        }
+    }
+}
---
+++ UnitTestProject1/PricingValueTests.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PricingEngine;
+
+namespace PricingEngineTests
+{
+    [TestClass]
+    public class PricingValueTests
+    {
+        [TestMethod]
+        public void CalculateUnitPrice_BelowMinPrice()
+        {
+            // arrange
+            PricingEngine engine = new PricingEngine(false);
+            // act
+            decimal unitPrice = engine.CalculateUnitPrice(10, 0.50m);
+            // assert
+            Assert.AreEqual(unitPrice, 0.50m);
+
+        }
+
+        [TestMethod]
+        public void CalculateUnitPrice_MinPrice()
+        {
+            // arrange
+            PricingEngine engine = new PricingEngine(false);
+            // act
+            decimal unitPrice = engine.CalculateUnitPrice(10, 1.00m);
+            // assert
+            Assert.AreEqual(unitPrice, 1.000m);
+        }
+
+        [TestMethod]
+        public void CalculateUnitPrice_BelowMinQty()
+        {
+            // arrange
+            PricingEngine engine = new PricingEngine(false);
+            // act
+            decimal unitPrice = engine.CalculateUnitPrice(5, 2.00m);
+            // assert
+            Assert.AreEqual(unitPrice, 2.000m);
+        }
+
+        [TestMethod]
+        public void CalculateUnitPrice_MinQtyLower()
+        {
+            // arrange
+            PricingEngine engine = new PricingEngine(false);
+            // act
+            decimal unitPrice = engine.CalculateUnitPrice(10, 2.00m)
+            // assert
+            Assert.AreEqual(unitPrice, 2.000m);
+        }
+
+        [TestMethod]
+        public void CalculateUnitPrice_MinQtyUpper()
+        {
+            // arrange
+            PricingEngine engine = new PricingEngine(false);
+            // act
+            decimal unitPrice = engine.CalculateUnitPrice(20, 2.00m)
+            // assert
+            Assert.AreEqual(unitPrice, 2.000m*9/10);
+        }
+
+        [TestMethod]
+        public void CalculateUnitPrice_QtyGT20()
+        {
+            // arrange
+            PricingEngine engine = new PricingEngine(false);
+            // act
+            decimal unitPrice = engine.CalculateUnitPrice(25, 2.00m)
+            // assert
+            Assert.AreEqual(unitPrice, 2.000m*8/10);
+        }
+
+        [TestMethod]
+        public void CalculateUnitPrice_HolidayBelowMinPrice()
+        {
+            // arrange
+            PricingEngine engine = new PricingEngine(true);
+            // act
+            decimal unitPrice = engine.CalculateUnitPrice(10, 0.50m);
+            // assert
+            Assert.AreEqual(unitPrice, 0.500m);
+        }
+
+        [TestMethod]
+        public void CalculateUnitPrice_HolidayBelowMinTotal()
+        {
+            // arrange
+            PricingEngine engine = new PricingEngine(true);
+            // act
+            decimal unitPrice = engine.CalculateUnitPrice(25, 2.00m);
+            // assert
+            Assert.AreEqual(unitPrice, 2.000m*8/10);
+        }
+
+        [TestMethod]
+        public void CalculateUnitPrice_HolidayBelowMinQty()
+        {
+            // Test the discount for holiday and quantity = 10 and total
+            // discounted amount is above the holiday threshold
+            PricingEngine engine = engine.CalculateUnitPrice(true);
+            // arrange
+            decimal unitPrice = engine.CalculateUnitPrice(10, 2.00m);
+            // act
+
+            // assert
+            Assert.AreEqual(unitPrice, 2.00m);
+        }
+
+        [TestMethod]
+        public void CalculateUnitPrice_HolidayMinQty()
+        {
+            // Test the discount for holiday and quantity = 10 and total
+            // discounted amount is above the holiday threshold
+            PricingEngine engine = engine.CalculateUnitPrice(true);
+            // arrange
+            decimal unitPrice = engine.CalculateUnitPrice(10, 120.00m);
+            // act
+
+            // assert
+            Assert.AreEqual(unitPrice, 120.000m*9/10);
+        }
+    }
+}
+
