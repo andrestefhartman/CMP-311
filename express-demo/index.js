01eac1288f78fde2b523d6b29e3c5ad74c5cb26b
Andre Hartman <andre.hartman@doane.edu>
2024-11-04 10:22:22 -0600

    New Commits, truck load

--- express-demo/index.js
+++ express-demo/index.js
@@ -4,71 +4,74 @@ const app = express();
 
 app.use(express.json());
 
-const courses = [
-  {id: 1, name: 'course1'},
-  {id: 2, name: 'course2'},
-  {id: 3, name: 'course3'},
+const rifles = [
+  {id: 1, caliber: '308', action: 'bolt'},
+  {id: 2, caliber: '30-06', action: 'bolt'},
+  {id: 3, caliber: '223', action: 'bolt'},
 ]
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/api/courses', (req, res) =>{
-  res.send(courses);
+app.get('/api/rifles', (req, res) =>{
+  res.send(rifles);
 });
 
-app.get('/api/courses/:id', (req, res) =>{
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('The course with the given ID was not found');
-  res.send(course);
+app.get('/api/rifles/:id', (req, res) =>{
+  const rifle = rifles.find(c => c.id === parseInt(req.params.id));
+  if (!rifle) res.status(404).send('The rifle with the given ID was not found');
+  res.send(rifle);
 });
 
-app.post('/api/courses', (req, res) =>{
-  const result = validateCourse(req.body);
-  const {error} = validateCourse(req.body);
+app.post('/api/rifles', (req, res) =>{
+  const result = validateRifle(req.body);
+  const {error} = validateRifle(req.body);
 
   if (error) return res.status(400).send(error.details[0].message);
     
   
 
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
+  const rifle = {
+    id: rifles.length + 1,
+    caliber: req.body.caliber,
+    action: req.body.action
   };
-  courses.push(course);
-  res.send(course);
+  rifles.push(rifle);
+  res.send(rifle);
 });
 
-app.put('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send('The course with the given ID was not found');
+app.put('/api/rifles/:id', (req, res) => {
+  const rifle = rifles.find(c => c.id === parseInt(req.params.id));
+  if (!rifle) return res.status(404).send('The rifle with the given ID was not found');
 
-  const result = validateCourse(req.body);
-  const {error} = validateCourse(req.body);
+  const result = validateRifle(req.body);
+  const {error} = validateRifle(req.body);
   if (error) return res.status(400).send(error.details[0].message);
     
 
 
-  course.name = req.body.name;
-  res.send(course)
+  rifle.caliber = req.body.caliber;
+  rifle.action = req.body.action;
+  res.send(rifle)
 })
 
-app.delete('/api/courses/:id', (req,res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send('The course with the given ID was not found');
+app.delete('/api/rifles/:id', (req,res) => {
+  const rifle = rifles.find(c => c.id === parseInt(req.params.id));
+  if (!rifle) return res.status(404).send('The rifle with the given ID was not found');
 
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
+  const index = rifles.indexOf(rifle);
+  rifles.splice(index, 1);
 
-  res.send(course);
+  res.send(rifle);
 });
 
-function validateCourse(course) {
+function validateRifle(rifle) {
   const schema = {
-    name: Joi.string().min(3).required()
+    caliber: Joi.string().min(1).required(),
+    action: Joi.string().min(1).required()
   };
-  return Joi.validate(course, schema);
+  return Joi.validate(rifle, schema);
 }
 
 
